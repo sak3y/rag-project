0eb08a23fbc4fd9a55b51aefe873dc6fa5f078e6
Rayhan A <sheikh36ahmed@gmail.com>
2026-09-14 22:44:40 +0100

    added search api and tested -> does work

--- api.py
+++ api.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import rag
 
 app = FastAPI()
-
+store = rag.get_store()
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    return {
+        "chunks": store._collection.count()
+    }
+
+@app.get("/search")
+def search(question: str, k_neighbours: int = 4):
+    hits = rag.retrieve(store, question, k_neighbours)
+    return {
+        "query": question,
+        "res": [d.page_content for d, _ in hits]
+    }
