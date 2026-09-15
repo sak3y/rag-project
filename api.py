5a1c331785bc9aab6c029acb6020ec6aecead368
Rayhan A <sheikh36ahmed@gmail.com>
2026-09-15 19:32:29 +0100

    added ask endpoint and fixed env issues with cache

--- api.py
+++ api.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
-import rag
+from fastapi import FastAPI, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
+import rag # connects to pipeline
+
+
+# Config
 app = FastAPI()
 store = rag.get_store()
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 @app.get("/health")
 def health():
@@ -11,9 +21,9 @@ def health():
     }
 
 @app.get("/search")
-def search(question: str, k_neighbours: int = 4):
+def search(query: str, k_neighbours: int = 4):
     # find the source within the vectore store
-    hits = rag.retrieve(store, question, k_neighbours)
+    hits = rag.retrieve(store, query, k_neighbours)
     res = []
     for doc, score in hits:
         res.append({
@@ -22,6 +32,12 @@ def search(question: str, k_neighbours: int = 4):
             "score": score
         })
     return {
-        "query": question,
+        "query": query,
         "res": res
     }
+
+@app.get("/ask")
+@limiter.limit("3/minute")
+def ask(request: Request, query: str, k: int = 4):
+    answer, hits = rag.ask(store, query, k)
+    return {"answer": answer}
